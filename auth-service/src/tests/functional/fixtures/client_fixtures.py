e009d793c438a73efd2fe14a9e23479fb94b115c
Tebald <5stilet5@gmail.com>
2024-04-25 18:54:15 +0300

    Refactored tests code.

--- auth-service/src/tests/functional/fixtures/client_fixtures.py
+++ auth-service/src/tests/functional/fixtures/client_fixtures.py
@@ -14,16 +14,14 @@ async def client_session() -> aiohttp.ClientSession:
 
 @pytest_asyncio.fixture(name='api_make_get_request')
 def api_make_get_request(client_session):
-    # @backoff.on_exception(backoff.expo, Exception, max_time=30, jitter=backoff.random_jitter)
-    async def inner(query_data: dict, endpoint: str):
+    @backoff.on_exception(backoff.expo, Exception, max_time=30, jitter=backoff.random_jitter)
+    async def inner(endpoint: str, query_data: dict = None, headers: dict = None):
         """
-        :param query_data: {'query': 'The Star', 'page_number': 1, 'page_size': 50}
-        :param endpoint: '/api/v1/account/{user_id}'
-        :return:
+        Send get request using aiohttp.
         """
         url = f'http://{test_base_settings.service_host}:{test_base_settings.service_port}'
         url += endpoint
-        async with client_session.get(url, params=query_data) as response:
+        async with client_session.get(url, params=query_data, headers=headers) as response:
             body = await response.json()
             status = response.status
         return status, body
@@ -33,14 +31,32 @@ def api_make_get_request(client_session):
 
 @pytest_asyncio.fixture(name='api_make_post_request')
 def api_make_post_request(client_session):
-    # @backoff.on_exception(backoff.expo, Exception, max_time=30, jitter=backoff.random_jitter)
+    @backoff.on_exception(backoff.expo, Exception, max_time=30, jitter=backoff.random_jitter)
     async def inner(query_data: dict, endpoint: str, headers: dict = None):
         """
-        Send post request using aiohttp.
+        Send post request using aiohttp with form data body.
         """
         url = f'http://{test_base_settings.service_host}:{test_base_settings.service_port}'
         url += endpoint
         async with client_session.post(url, data=query_data, headers=headers) as response:
+            body = await response.json()
+            status = response.status
+            rsp_headers = response.headers
+        return status, body, rsp_headers
+
+    return inner
+
+
+@pytest_asyncio.fixture(name='api_post')
+def api_post(client_session):
+    @backoff.on_exception(backoff.expo, Exception, max_time=30, jitter=backoff.random_jitter)
+    async def inner(body: dict, endpoint: str, headers: dict = None):
+        """
+        Send post request using aiohttp with json body.
+        """
+        url = f'http://{test_base_settings.service_host}:{test_base_settings.service_port}'
+        url += endpoint
+        async with client_session.post(url, json=body, headers=headers) as response:
             body = await response.json()
             status = response.status
             rsp_headers = response.headers
--- auth-service/src/tests/functional/fixtures/pg_fixtures.py
+++ auth-service/src/tests/functional/fixtures/pg_fixtures.py
@@ -1,25 +1,20 @@
 import pytest_asyncio
 import backoff
-from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
-                                    create_async_engine)
-import uuid
-from datetime import UTC, datetime
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine)
 
-from sqlalchemy import (Boolean, Column, DateTime, ForeignKey, String,
-                        UniqueConstraint, insert, delete)
-from sqlalchemy.dialects.postgresql import UUID
-from werkzeug.security import check_password_hash, generate_password_hash
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import insert
 
 from src.tests.functional.settings import test_base_settings as settings
-
 from src.models.db_entity import (
     User,
-    Role, 
-    Permission, 
-    RolePermission
+    Role,
+    Permission,
+    RolePermission,
+    Base
 )
-from src.db.postgres import Base
 
 
 dsn = f'postgresql+asyncpg://{settings.pg_user}:{settings.pg_password}@{settings.pg_host}:{settings.pg_port}/{settings.pg_db}'
@@ -27,41 +22,15 @@ engine = create_async_engine(dsn, echo=False, future=True)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-@pytest_asyncio.fixture(name='db_session', scope='session')
-# @backoff.on_exception(backoff.expo, Exception, max_time=30, jitter=backoff.random_jitter)
-async def db_session() -> AsyncSession:
-    async with async_session() as session:
-        yield session
-
-
-@pytest_asyncio.fixture(name='pg_create_tables')
-async def pg_create_tables():
-    # @backoff.on_exception(backoff.expo, Exception, max_time=30, jitter=backoff.random_jitter)
-    async def inner():
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
-
-    return inner
-
-
-@pytest_asyncio.fixture(name='pg_clear_all')
-# @backoff.on_exception(backoff.expo, Exception, max_time=30, jitter=backoff.random_jitter)
-async def pg_clear_all():
-    async def inner():
-        # print(f'CURRENT FOLDER: {settings.current_folder}')
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
-
-    return inner
-
-
 @pytest_asyncio.fixture(name='pg_insert_table_data')
 def pg_insert_table_data():
-    # @backoff.on_exception(backoff.expo, Exception, max_time=30, jitter=backoff.random_jitter)
+    @backoff.on_exception(backoff.expo, Exception, max_time=30, jitter=backoff.random_jitter)
     async def inner(table_name, data: dict):
         async with engine.begin() as conn:
+
             statement = (
-                insert(table_name).values(**data)
+                insert(table_name).
+                values(**data)
             )
             await conn.execute(statement=statement)
             await conn.commit()
@@ -69,140 +38,14 @@ def pg_insert_table_data():
     return inner
 
 
-@pytest_asyncio.fixture(name='pg_drop_table_data')
-def pg_drop_table_data():
-    # @backoff.on_exception(backoff.expo, Exception, max_time=30, jitter=backoff.random_jitter)
-    async def inner(table_name):
+@pytest_asyncio.fixture(name='pg_clear_tables_data')
+def pg_clear_tables_data():
+    @backoff.on_exception(backoff.expo, Exception, max_time=30, jitter=backoff.random_jitter)
+    async def inner():
+        """Function to clean DB tables"""
         async with engine.begin() as conn:
-            statement = (
-                delete(table_name)
-            )
-            await conn.execute(statement=statement)
+            for table in reversed(Base.metadata.sorted_tables):
+                await conn.execute(table.delete())
             await conn.commit()
 
     return inner
-
-
-@pytest_asyncio.fixture(name="super_user")
-async def super_user(db_session):
-    user = User(email="su@example.com", hashed_password="qwerty", is_superuser=True)
-    db_session.add(user)
-    await db_session.commit()
-    await db_session.refresh(user)
-    yield user
-    await db_session.delete(user)
-    await db_session.commit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Base(DeclarativeBase):
-    pass
-
-
-class UUIDMixin:
-    # Points that we should not create such table in DB.
-    __abstract__ = True
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-
-
-class User(UUIDMixin, Base):
-    """
-    Class to represent DB 'users' table data model
-    """
-    __tablename__ = 'users'
-    email = Column(String(255), unique=True, nullable=False)
-    hashed_password = Column(String(1024), nullable=False)
-    is_active = Column(Boolean, default=True, nullable=False)
-    is_superuser = Column(Boolean, default=False, nullable=False)
-    is_verified = Column(Boolean, default=False, nullable=False)
-    registered_at = Column(DateTime(timezone=True), default=datetime.now(UTC))
-
-    def __init__(self, email: str,
-                 hashed_password: str,
-                 is_active: bool | None = None,
-                 is_superuser: bool | None = None,
-                 is_verified: bool | None = None,
-                 registered_at: datetime | None = None) -> None:
-        super().__init__()
-        self.email = email
-        self.hashed_password = self.hashed_password = generate_password_hash(hashed_password)
-        self.is_active = is_active
-        self.is_superuser = is_superuser
-        self.is_verified = is_verified
-        self.registered_at = registered_at
-
-    async def check_password(self, password: str) -> bool:
-        return check_password_hash(self.hashed_password, password)
-
-    @staticmethod
-    async def get_password_hashed(password) -> str:
-        return generate_password_hash(password)
-
-    def __repr__(self) -> str:
-        return f'<User {self.email}>'
-
-
-class Role(UUIDMixin, Base):
-    """
-    Class to represent DB 'roles' table data model
-    """
-    __tablename__ = 'roles'
-
-    name = Column(String(1024), unique=True, nullable=False)
-
-
-class Permission(UUIDMixin, Base):
-    """
-    Class to represent DB 'permissions' table data model
-    """
-    __tablename__ = 'permissions'
-
-    name = Column(String(1024), unique=True, nullable=False)
-
-
-class RolePermission(UUIDMixin, Base):
-    """
-    Class to represent DB 'role_permissions' table data model
-    """
-    __tablename__ = 'role_permissions'
-
-    role_id = Column(UUID(as_uuid=True), ForeignKey("roles.id"), nullable=False)
-    permission_id = Column(UUID(as_uuid=True), ForeignKey("permissions.id"), nullable=False)
-    __table_args__ = (UniqueConstraint('role_id', 'permission_id', name='_role_permission_unic'),)
-
-
-class UserRole(UUIDMixin, Base):
-    """
-    Class to represent DB 'user_roles' table data model
-    """
-    __tablename__ = 'user_roles'
-
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
-    role_id = Column(UUID(as_uuid=True), ForeignKey("roles.id"), nullable=False)
-    __table_args__ = (UniqueConstraint('user_id', 'role_id', name='_user_role_unic'),)
-
-
-class LoginHistory(UUIDMixin, Base):
-    """
-    Class to represent DB 'login_history' table data model
-    """
-    __tablename__ = 'login_history'
-
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
-    timestamp = Column(DateTime)
-    ip_address = Column(String(15))
-    location = Column(String(255))
-    user_agent = Column(String(255))
