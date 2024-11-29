0decaccf1f6e7bc9af4a5344cc59c166e1cdc748
Pavel Klimochkin <28781744+Tebald@users.noreply.github.com>
2024-11-29 18:11:14 +0300

    Updated README.md for better readability. (#22)
    Fixed tests run in the container mode.
    Updated service and tests settings env file usage.
    Added service scheme image to the docs.

--- auth-service/src/tests/functional/settings.py
+++ auth-service/src/tests/functional/settings.py
@@ -6,29 +6,27 @@ import os
 from pydantic import ConfigDict, Field
 from pydantic_settings import BaseSettings
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
+DOTENV = os.path.join(os.path.dirname(__file__), 'test.env')
 
 
 class TestBaseSettings(BaseSettings):
     model_config = ConfigDict(extra='ignore')
     # Postgres
-    pg_db: str = Field('', env='PG_DB')
-    pg_user: str = Field('', env='PG_USER')
-    pg_password: str = Field('', env='PG_PASSWORD')
-    pg_host: str = Field('127.0.0.1', env='PG_HOST')
-    pg_port: int = Field(5433, env='PG_PORT')
+    pg_db: str = Field('', alias='PG_DB')
+    pg_user: str = Field('', alias='PG_USER')
+    pg_password: str = Field('', alias='PG_PASSWORD')
+    pg_host: str = Field('127.0.0.1', alias='PG_HOST')
+    pg_port: int = Field(5433, alias='PG_PORT')
     # Redis
-    redis_host: str = Field('127.0.0.1', env='REDIS_HOST')
-    redis_port: int = Field(6379, env='REDIS_PORT')
+    redis_host: str = Field('127.0.0.1', alias='REDIS_HOST')
+    redis_port: int = Field(6379, alias='REDIS_PORT')
 
-    service_host: str = Field('0.0.0.0', env='API_HOST')
-    service_port: int = Field(8000, env='API_PORT')
+    service_host: str = Field('0.0.0.0', alias='API_HOST')
+    service_port: int = Field(8000, alias='API_PORT')
     # JWT token
-    jwt_secret_key: str = Field(env='JWT_SECRET_KEY')
-    jwt_algorithm: str = Field(env='JWT_ALGORITHM')
-    jwt_at_expire_minutes: int = Field(env='JWT_AT_EXPIRE_MINUTES')
-
-    current_folder: str = Field(current_dir)
+    jwt_secret_key: str = Field(alias='JWT_SECRET_KEY')
+    jwt_algorithm: str = Field(alias='JWT_ALGORITHM')
+    jwt_at_expire_minutes: int = Field(alias='JWT_AT_EXPIRE_MINUTES')
 
 
-test_base_settings = TestBaseSettings(_env_file='../../../../.env', _env_file_encoding='utf-8')
+test_base_settings = TestBaseSettings(_env_file=DOTENV, _env_file_encoding='utf-8')
