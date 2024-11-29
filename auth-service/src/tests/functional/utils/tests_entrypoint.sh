6ceb26eee96311b59c3eebc902012b9679f36f60
Tebald <5stilet5@gmail.com>
2024-11-29 18:07:06 +0300

    Updated README.md for better readability. Fixed tests run in the container mode. Updated service and tests settings env file usage. Added service scheme image to the docs.

--- auth-service/src/tests/functional/utils/tests_entrypoint.sh
+++ auth-service/src/tests/functional/utils/tests_entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Ожидание доступности Postgres и Redis..."
-python ./tests/functional/utils/wait_for_services.py
+python ./src/tests/functional/utils/wait_for_services.py
 
 if [ $? -eq 0 ]; then
     echo "Сервисы доступны. Запуск тестов..."
-    pytest -rP ./tests/functional/src
+    pytest -rP ./src/tests/functional/src
 else
     echo "Ошибка при ожидании сервисов. Проверьте состояние сервисов и повторите попытку."
     exit 1
